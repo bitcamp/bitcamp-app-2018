dec417cdf3cd1f89b026dcd2443095c6d05aa56f
Tal Davidi <tal.davidi@gmail.com>
2018-04-04 15:56:58 -0400

    Add release version of apk

--- bitcamp2018/ios/bitcamp2018/AppDelegate.m
+++ bitcamp2018/ios/bitcamp2018/AppDelegate.m
@@ -23,7 +23,8 @@
   
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"bitcamp2018"
