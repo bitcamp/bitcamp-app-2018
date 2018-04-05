63fe23c4bf92a4826f355d34d25c8c4d350fb069
Tal Davidi <tal.davidi@gmail.com>
2018-04-05 15:30:47 -0400

    Prepare release with no progauard

--- bitcamp2018/ios/bitcamp2018/AppDelegate.m
+++ bitcamp2018/ios/bitcamp2018/AppDelegate.m
@@ -24,8 +24,8 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-
-
+  //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"bitcamp2018"
                                                initialProperties:nil
